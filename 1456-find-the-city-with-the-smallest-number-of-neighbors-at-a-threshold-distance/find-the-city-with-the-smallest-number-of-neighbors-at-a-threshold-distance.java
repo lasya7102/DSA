c847180f4e485b091297a28a14fdf0055c7c1a6a
lasya7102 <lasyanimma998@gmail.com>
2026-08-04 18:10:44 +0530

    Time: 8 ms (93.83%) | Memory: 46.5 MB (58.98%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -37,7 +37,7 @@ for (int i = 0; i < n; i++) {
             int count=0;
             for(int j=0;j<n;j++)
             {
-                if(dist[i][j]<=distanceThreshold) count++;
+                if(i!=j && dist[i][j]<=distanceThreshold) count++;
             }
            if(count<=min)
            {
