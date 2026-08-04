45ea64bee0841609b5647cf1aef1f584305a57a9
lasya7102 <lasyanimma998@gmail.com>
2026-08-04 12:01:49 +0530

    Time: 47 ms (10.07%) | Memory: 46.9 MB (16.48%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,49 +1,72 @@
-class Solution {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int dist[][]=new int[n][n];
-        int INF = 100000000;
-
-for (int i = 0; i < n; i++) {
-    Arrays.fill(dist[i], INF);
-    dist[i][i] = 0;
+class Pair
+{
+ int edge,dis;
+ Pair(int edge,int dis)
+ {
+    this.edge=edge;
+    this.dis=dis;
+ }
 }
+class Solution {
+    int min=Integer.MAX_VALUE;
+    ArrayList<Integer> ans=new ArrayList<>();
+     void dijsktras(ArrayList<ArrayList<Pair>> list,int src,int k)
+    {
+      int dist[]=new int[list.size()];
+      for(int i=0;i<dist.length;i++)
+      {
+        dist[i]=Integer.MAX_VALUE;
+      }
+      dist[src]=0;
+      PriorityQueue<Pair> pq=new PriorityQueue<>((x,y)->x.dis-y.dis);
+      pq.offer(new Pair(src,0));
+      while(!pq.isEmpty())
+      {
+       Pair p=pq.poll();
+        for(Pair e:list.get(p.edge))
+        {
+            if(e.dis+p.dis<dist[e.edge])
+            {
+                dist[e.edge]=e.dis+p.dis;
+                pq.offer(new Pair(e.edge,dist[e.edge]));
+            }
+        }
+      }
+      int count=0;
+      for(int ch:dist)
+      {
+        if(ch<=k) 
+        {
+            count++;
+        }
+      }
+      min=Math.min(min,count);
+      ans.add(count);
+    }
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        ArrayList<ArrayList<Pair>> list=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            list.add(new ArrayList<>());
+        }
         for(int i=0;i<edges.length;i++)
         {
             int u=edges[i][0];
             int v=edges[i][1];
-            int dis=edges[i][2];
-            dist[u][v]=dis;
-            dist[v][u]=dis;
+            int w=edges[i][2];
+            list.get(u).add(new Pair(v,w));
+            list.get(v).add(new Pair(u,w));
         }
-
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                for(int k=0;k<n;k++)
-                {
-                    if(dist[j][i]!=INF && dist[i][k]!=INF && dist[j][i]+dist[i][k]<dist[j][k])
-                    {
-                        dist[j][k]=dist[j][i]+dist[i][k];
-                    }
-                }
-            }
-        }
-        ArrayList<Integer> list=new ArrayList<>();
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<n;i++)
-        {
-            int count=0;
-            for(int j=0;j<n;j++)
-            {
-                if(i!=j && dist[i][j]<=distanceThreshold) count++;
-            }
-            min=Math.min(min,count);
-            list.add(count);
+            dijsktras(list,i,distanceThreshold);
         }
         for(int i=n-1;i>=0;i--)
         {
-            if(list.get(i)==min) return i;
+            if(ans.get(i)==min)
+            {
+                return i;
+            }
         }
         return 0;
     }
