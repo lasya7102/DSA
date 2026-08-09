a0a749550d4e7897d1044a06edfc9418acb8255c
lasya7102 <lasyanimma998@gmail.com>
2026-08-09 12:07:28 +0530

    Time: 56 ms (35.43%) | Memory: 146.7 MB (73.50%) - LeetSync

--- 4352-weighted-sum-of-a-tree/weighted-sum-of-a-tree.java
+++ 4352-weighted-sum-of-a-tree/weighted-sum-of-a-tree.java
@@ -1,22 +1,5 @@
 class Solution {
-     int height=1;
-       
-     void dfs(int i,ArrayList<ArrayList<Integer>> list,int depth[])
-    {
-       
-        
-        for(int ch:list.get(i))
-        {
-             depth[ch] = depth[i] + 1;
-            height = Math.max(height, depth[ch]);
-
-            dfs(ch, list, depth);
-        }
-        
-        
-        
-          
-    }
+    
     public long weightedSum(int[] parent, int[] nums) {
       
        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
@@ -28,23 +11,22 @@ class Solution {
             {
                 list.get(parent[i]).add(i);
             }
+        int height=1;
        
         int depth[]=new int[parent.length];
          depth[0]=1;
-
-        // Queue<Integer> queue=new LinkedList<>();
-        // queue.offer(0);
-        // while(!queue.isEmpty())
-        //     {
-        //         int x=queue.poll();
-        //         for(int ch:list.get(x))
-        //             {
-        //                 depth[ch]=depth[x]+1;
-        //                 height=Math.max(depth[ch],height);
-        //                 queue.offer(ch);
-        //             }
-        //     }
-        dfs(0,list,depth);
+        Queue<Integer> queue=new LinkedList<>();
+        queue.offer(0);
+        while(!queue.isEmpty())
+            {
+                int x=queue.poll();
+                for(int ch:list.get(x))
+                    {
+                        depth[ch]=depth[x]+1;
+                        height=Math.max(depth[ch],height);
+                        queue.offer(ch);
+                    }
+            }
         long ans=0;
         for(int i=0;i<parent.length;i++)
             {
